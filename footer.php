2bc67cacd84c04f05a1a1b102a0fb35b5d1cf8fc
Felix Frederick <corderofelix101@gmail.com>
2021-02-05 12:45:33 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+/* footer Felix-z.com Feb. 5,2021 12:43 PM*/
+
+</div>
+
+<footer id="footer">
+
+	<div id="copyright">
+		&copy; <?php echo esc_html( date_i18n( __( 'Y', 'Felix Cordero Jr.' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+
+</div>
+
+	</footer>
+
+</div>
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
